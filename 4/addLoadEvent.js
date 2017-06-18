7305f1c247f03ca043f86becc193d0622376008d
高青青“git config --global user.email gaoqingqing@gridsum.comgit clone git@gitlab.gridsum.com:gov_law_tech/xiangjing-web.gitcd xiangjing-webtouch README.mdgit add README.mdgit commit -m add <gaoqingqing@gridsum.com>
2017-06-18 20:05:04 +0800

    Ajax

---
+++ 4/addLoadEvent.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Lenovo on 2017/6/18.
+ */
+
+// function addLoaEvent(func) {
+//     var oldonload = window.onload;
+//     if (typeof window.onload != 'function') {
+//         window.onload = func;
+//     }else {
+//         window.onload = function () {
+//             onload();
+//             func();
+//         }
+//     }
+// }
